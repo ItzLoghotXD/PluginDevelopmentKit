08e2fe3c5cbd85a1decfb510713db5e56a95b5f3
ItzLoghotXD <loghot.gamerz.official@gmail.com>
2025-03-02 12:11:06 +0530

    jitpack fix.

--- src/test/java/TestPlugin.java
+++ src/test/java/TestPlugin.java
@@ -1,2 +1,12 @@
-public class TestPlugin {
+import me.itzloghotxd.pdk.utilities.chat.ChatUtilities;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TestPlugin extends JavaPlugin {
+    private ChatUtilities chatUtilities;
+
+    public void onEnable() {
+        chatUtilities = new ChatUtilities();
+
+        chatUtilities.broadcastMessage("hi!");
+    }
 }
