90ff6d69a35c004c155b0a8565e43d3474983568
ItzLoghotXD <loghot.gamerz.official@gmail.com>
2026-05-13 22:14:38 +0530

    add SubCommand and CommandManager for handling subcommand

--- command/src/main/java/me/itzloghotxd/pdk/command/CommandHandler.java
+++ command/src/main/java/me/itzloghotxd/pdk/command/CommandHandler.java
@@ -10,22 +10,29 @@
 
 package me.itzloghotxd.pdk.command;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Objects;
 
 /**
- * Handles command execution for a specific registered command.
+ * Handles command execution and tab completion for a specific registered command.
+ * It delegates command handling and tab completion to {@link CommandManager}.
  *
  * @author ItzLoghotXD
  * @since 0.1.0
  */
-public class CommandHandler implements CommandExecutor {
+public class CommandHandler implements CommandExecutor, TabExecutor {
 
+    private final String name;
+    private final CommandManager commandManager;
     private final BaseCommandExecutor baseExecutor;
 
     /**
@@ -33,19 +40,37 @@ public class CommandHandler implements CommandExecutor {
      *
      * @param plugin The plugin instance registering this command handler.
      * @param name The name of the command being handled.
+     * @param commandManager The {@link CommandManager} instance.
      * @param baseExecutor The {@link BaseCommandExecutor} implementation.
      */
-    public CommandHandler(@NotNull JavaPlugin plugin, @NotNull String name, @NotNull BaseCommandExecutor baseExecutor) {
+    public CommandHandler(@NotNull JavaPlugin plugin, @NotNull String name, @NotNull CommandManager commandManager, @Nullable BaseCommandExecutor baseExecutor) {
+        this.commandManager = commandManager;
+        this.name = name;
         this.baseExecutor = baseExecutor;
         if (plugin.getCommand(name) == null) {
             plugin.getLogger().severe("[PDK COMMAND] The command is null.");
             return;
         }
         Objects.requireNonNull(plugin.getCommand(name)).setExecutor(this);
+        Objects.requireNonNull(plugin.getCommand(name)).setTabCompleter(this);
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        return baseExecutor.execute(sender, command, label, args);
+        if (args.length == 0) {
+            if (baseExecutor != null) {
+                return baseExecutor.execute(sender, command, label, args);
+            }
+
+            sender.sendMessage(ChatColor.WHITE + "Usage: /" + name + " <subcommand>");
+            return true;
+        }
+
+        return commandManager.execute(sender, args);
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        return commandManager.getTabCompletions(sender, args);
     }
 }
